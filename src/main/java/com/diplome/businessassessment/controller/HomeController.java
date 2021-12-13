b2d59ccdcd07fde71b03a1898a322bc458948168
Mykyta <Mykyta_Leonov@epam.com>
2021-12-13 20:16:46 +0200

    About partners page

--- src/main/java/com/diplome/businessassessment/controller/HomeController.java
+++ src/main/java/com/diplome/businessassessment/controller/HomeController.java
@@ -1,34 +1,18 @@
 package com.diplome.businessassessment.controller;
 
-import com.diplome.businessassessment.helper.AssessmentHelper;
-import com.diplome.businessassessment.repository.MetricRepository;
-import com.diplome.businessassessment.repository.FunctionalityRepository;
-import com.diplome.businessassessment.repository.SystemRepository;
-import com.diplome.businessassessment.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private FunctionalityRepository functionalityRepository;
-
-    @Autowired
-    private SystemRepository systemRepository;
-
-    @Autowired
-    private MetricRepository metricRepository;
-
-    @Autowired
-    private AssessmentHelper helper;
-
     @RequestMapping("/")
     public String homePage() {
         return "index";
+    }
+
+    @RequestMapping("/partners")
+    public String partnersPage() {
+        return "partners";
     }
 }
